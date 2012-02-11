b7521a97d1be0a78e409fd44e4585a98a6813b1a
Fletcher Liverance <fletcher.liverance@gmail.com>
2012-02-11 21:47:36 +0000

    Wipe out micropost so we can restart

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Micropost::Application.config.secret_token = 'f68c6f5cb58ce2f2e243266a84132403b7ac8da53ed1580d45b301bd30a1e94cb1fa9532de8e5f5e4e763240b4f2f1f413b061138334a7308b9d9c2568626ac2'
--- test/unit/helpers/micropost_news_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class MicropostNewsHelperTest < ActionView::TestCase
-end
