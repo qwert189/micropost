fc81388bf36df43fa6372087cabe3470068ea6d0
Fletcher Liverance <fletcher.liverance@gmail.com>
2012-02-11 21:54:54 +0000

    Initial thesis webpage commit, based on micropost scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Thesis::Application.config.secret_token = 'a5939a156125d8b10eff0244a7d315afabf822bbe42bf3fbf389180234a0bd51b4dc188998c9923a30330fc274d143b865fb9b73e42dd91e29b5eaef183de55d'
